f2a16a28c178c1000d00f3af16569366d3426185
marmooo <marmooo@users.noreply.github.com>
2022-07-24 09:06:36 +0900

    bump bootstrap from 5.1.3 to 5.2.0

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2022-05-21 21:00';
+var CACHE_NAME = "2022-07-24 00:46";
 var urlsToCache = [
   "/flags-quiz/",
   "/flags-quiz/index.js",
@@ -9,8 +9,8 @@ var urlsToCache = [
   "/flags-quiz/mp3/incorrect1.mp3",
   "/flags-quiz/mp3/correct3.mp3",
   "/flags-quiz/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js",
   "https://cdn.jsdelivr.net/npm/three@0.102.1/build/three.min.js",
   "https://cdn.jsdelivr.net/npm/giojs@2.2.2/build/gio.min.js",
 ];
