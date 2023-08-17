d846dfac51e3796fa46e9c4fc3f484bc064c4805
marmooo <marmooo@users.noreply.github.com>
2023-08-18 00:07:04 +0900

    fix

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "2023-08-18 00:05";
+const CACHE_NAME = "2023-08-18 00:10";
 const urlsToCache = [
   "/flags-quiz/",
   "/flags-quiz/index.js",
@@ -6,6 +6,7 @@ const urlsToCache = [
   "/flags-quiz/mp3/incorrect1.mp3",
   "/flags-quiz/mp3/correct3.mp3",
   "/flags-quiz/favicon/favicon.svg",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js",
   "https://cdn.jsdelivr.net/npm/three@0.102.1/build/three.min.js",
   "https://cdn.jsdelivr.net/npm/giojs@2.2.2/build/gio.min.js",
 ];
